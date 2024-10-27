3b34b478d7c142a9e6895c38d0a9fb05571a1dc4
renolCrisAngel <78592535+renolCrisAngel@users.noreply.github.com>
2024-10-27 16:57:12 +0800

    Add files via upload

---
+++ 24HourCodeChallenge/script.js
@@ -0,0 +1,15 @@
+function openShoppingBagModal() {
+    document.getElementById("shoppingBagModal").style.display = "block";
+}
+
+function closeShoppingBagModal() {
+    document.getElementById("shoppingBagModal").style.display = "none";
+}
+
+function openCheckOutModal() {
+    document.getElementById("checkOutModal").style.display = "block";
+}
+
+function closeCheckOutModal() {
+    document.getElementById("checkOutModal").style.display = "none";
+}
